6da6f215719f1874081ff71c5ae4edbd32c828bb
Ivan Gadžić <83777643+geki7@users.noreply.github.com>
2023-05-19 18:35:35 +0200

    Add files via upload

---
+++ Auti zavrsna vjezba/Auti/Cars.item.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Auti
+{
+    public partial class Car
+    {
+        public double TotalPrice;
+
+        public double MyProperty
+        {
+            get { return TotalPrice = CalculateTotalPrice(); }
+            set { TotalPrice = value; }
+        }
+
+
+        public double CalculateTotalPrice()
+        {
+            if (this.Power <= 100)
+            {
+                TotalPrice = this.Price * 1.1;
+            }
+            else
+            {
+                TotalPrice = this.Price * 1.25;
+            }
+            return TotalPrice;
+        }
+    }
+}
---
+++ Auti zavrsna vjezba/Auti/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Auti
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            FillProizvodac();
+        }
+
+        public void FillProizvodac()
+        {
+            using(var context = new Labs2022Entities())
+            {
+                var query = from p in context.Cars
+                            select p.Manufacturer;
+
+                comboBox1.DataSource = query.Distinct().ToList();
+            }
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string odabrani = comboBox1.SelectedItem.ToString();
+
+            using (var context = new Labs2022Entities())
+            {
+                var query = from p in context.Cars
+                            where p.Manufacturer == odabrani
+                            select p.Model;
+
+                comboBox2.DataSource = query.Distinct().ToList();
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string odabrani = comboBox1.SelectedItem.ToString();
+            string odabranimodel = comboBox2.SelectedItem.ToString();
+
+            using (var context = new Labs2022Entities())
+            {
+                var query = from p in context.Cars
+                            where p.Manufacturer == odabrani && p.Model == odabranimodel
+                            select p;
+
+                dataGridView1.DataSource = query.ToList();
+            }
+
+        }
+    }
+}
